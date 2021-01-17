408993ad2dc35a654558e5e8fe5f729b54c8c6a8
Ana Livia <analiviacastro34@gmail.com>
2021-01-17 01:02:10 -0300

    tema dark

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function escuro() {
+    var tescuro = window.document.getElementById('corpo')
+    var pescuro = window.document.getElementById('ptheme')
+    tescuro.style.background = 'rgb(53, 54, 58)'
+    tescuro.style.color = 'white'
+    pescuro.style.color = 'white'
+}
+
+function claro() {
+    var tclaro = window.document.getElementById('corpo')
+    var pclaro = window.document.getElementById('ptheme')
+    tclaro.style.background = 'white'
+    tclaro.style.color = 'black'
+    pclaro.style.color = 'black'
+    
+}
